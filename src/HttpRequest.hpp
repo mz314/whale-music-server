aca852e27c4f2978bdef8c84f21393308d9a3e6b
Maciej Zakrzewski <maciej.zakrzewski87@gmail.com>
2015-09-11 15:27:03 +0200

    some tweaks

--- src/HttpRequest.hpp
+++ src/HttpRequest.hpp
@@ -2,14 +2,15 @@
 #define	HTTPREQUEST_HPP
 
 #include <string>
+#include <map>
 
 using namespace std;
 
 class HttpRequest {
 protected:
     string request_body;
-    reqParams parseRequest();
-    static void parseParams(string params);
+    void parseRequest();
+    static map<string, string> parseParams(string params);
 public:
     HttpRequest(string request_body);
     static string replaceEntities(string str);
